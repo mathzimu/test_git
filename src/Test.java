4891575768171f7959e3ae0477aa905ee9779a9c
ryan <ryan@dynadot.com>
2026-01-22 09:24:37 +0800

    add new test method1

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void test() {
+        System.out.println("Add new test method1");
+    }
 }
